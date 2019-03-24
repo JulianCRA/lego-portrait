1612039ae7a6d52d7b40c9d8649facf430378019
JulianCRA <admin@julianr.xyz>
2019-03-25 02:57:08 +0700

    Image resizing and aspect ratio logic

--- sketch.js
+++ sketch.js
@@ -6,11 +6,16 @@ window.onload = () => {
 	filedrop = document.getElementById('filedrop');
 	sketch = document.getElementById('sketch');
 
+	window.ondragover = (e) => e.preventDefault();
+	window.ondrop = (e) => e.preventDefault();
+	sketch.ondragover = (e) => e.preventDefault();
+	sketch.ondrop = (e) => e.preventDefault();
+
 	filedrop.onclick = handleClick;
 	filedrop.ondragover = handleDragOver;
 	filedrop.ondrop = handleDrop;
 
-	sketch.onclick = () => console.log("SKETCH CLIC")
+	
 }
 
 handleDragOver = (e) => {
@@ -23,8 +28,7 @@ handleDrop = (e) => {
 	if(item.kind === 'file'){
 		if (item.type.indexOf('image/') === 0) {
 			imageFile = URL.createObjectURL(item.getAsFile());
-			//new p5(sk, 'filedrop');
-			console.log(imageFile);
+			new p5(tetrisSketch, 'sketch');
 		}else{
 			console.log(item);
 			console.log('Images, please.');
@@ -37,7 +41,7 @@ handleDrop = (e) => {
 					success => {
 						console.log(success);
 						imageFile = data;
-						new p5(sk, 'sketch');
+						new p5(tetrisSketch, 'sketch');
 					}, 
 					() => console.log("Can't and/or won't")
 				);
@@ -59,8 +63,7 @@ handleClick = () => {
 			const reader = new FileReader();
 			reader.onload = (e) => {
 				imageFile = e.target.result; 
-				//new p5(sk, 'sketch');
-				console.log(imageFile);
+				new p5(tetrisSketch, 'sketch');
 			};
 			reader.readAsDataURL(file);
 		}else{
@@ -72,4 +75,53 @@ handleClick = () => {
 toggleHidden = () => {
 	filedrop.classList.toggle("hidden");
 	sketch.classList.toggle("hidden");
+}
+
+testImage = (url, timeoutT) => {
+	return new Promise(function (resolve, reject) {
+        var timeout = timeoutT || 5000;
+        var timer, img = new Image();
+        img.onerror = img.onabort = function () {
+            clearTimeout(timer);
+            reject("error");
+        };
+        img.onload = function () {
+            clearTimeout(timer);
+            resolve(img);
+        };
+        timer = setTimeout(function () {
+            // reset .src to invalid URL so it stops previous
+            // loading, but doesn't trigger new load
+            img.src = "//!!!!/test.jpg";
+            reject("timeout");
+        }, timeout);
+        img.src = url;
+    });
+}
+
+const tetrisSketch = (p) => {
+	let img;
+	p.preload = () => {
+		img = p.loadImage(imageFile, ()=>{}, 
+			e => {
+				const pic = e.target;
+				pic.crossOrigin = null, pic.src = pic.src;
+			}
+		);
+	}
+
+	p.setup = () => {
+		toggleHidden();
+		p.createCanvas(sketch.offsetWidth, sketch.offsetHeight);
+		p.noLoop();
+	}
+
+	p.draw = () => {
+		const ratio = Math.min(p.width/img.width, p.height/img.height);
+		p.image(img, (p.width-(img.width*ratio))/2, (p.height-(img.height*ratio))/2, img.width*ratio, img.height*ratio);
+	}
+
+	p.windowResized = () => {
+		p.resizeCanvas(sketch.offsetWidth, sketch.offsetHeight);
+	}
 }
